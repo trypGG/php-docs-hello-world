a9e331b2f9a25e079826c9c4e8a9afd7ebcbb2f7
trypGG <131827834+trypGG@users.noreply.github.com>
2025-05-09 12:33:58 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,33 +1,33 @@
 <?php
-
 echo "Hola a todos y todas, que tengan un excelente día 21051392";
+?>
 
+<!DOCTYPE html>
 <html>
 <head>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js" type="text/javascript" charset="utf-8"></script>
+    <title>Control On/Off</title>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
+    <script>
+        var accessToken = "747c9b3297d5640bd7ecfaa77ca1cd5d155e1bcc";
+        var deviceID = "0a10aced202194944a055b7c";
+        var url = "https://api.particle.io/v1/devices/" + deviceID + "/led";
 
-<script>
-var accessToken = "747c9b3297d5640bd7ecfaa77ca1cd5d155e1bcc";
-var deviceID = "0a10aced202194944a055b7c"
-var url = "https://api.particle.io/v1/devices/" + deviceID + "/led";
+        function switchOn() {
+            $.post(url, { params: "on", access_token: accessToken });
+        }
 
-function switchOn()
-{
-	$.post(url, {params: "on", access_token: accessToken });
-}  
-
-function switchOff()
-{
-	$.post(url, {params: "off", access_token: accessToken });
-}  
-
-</script>
-</head>	
-
+        function switchOff() {
+            $.post(url, { params: "off", access_token: accessToken });
+        }
+    </script>
+</head>
 <body>
+    <h1>On/Off Control</h1>	
+    <input type="button" onClick="switchOn()" value="ON"/>
+    <input type="button" onClick="switchOff()" value="OFF"/>
+</body>
+</html>
 
-<h1>On/Off Control</h1>	
-	
 <input type="button" onClick="switchOn()" value="ON"/>
 <input type="button" onClick="switchOff()" value="OFF"/>
 	
